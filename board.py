31a0b169f5e7e9ff8b439fb7f5fc06d5de6f1480
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-12-16 16:40:18 +0200

    Create board constructor with the upper player's store
    Needs tuning, the image is not the right place.

--- board.py
+++ board.py
@@ -1,5 +1,12 @@
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
 
 
 class Board(FloatLayout):
     """Class representing the board, with the graphics."""
+
+    def __init__(self):
+        super(Board, self).__init__(size=(1600, 200))
+        self.up_store = Image(source='images\\store.png', pos=(0, 0), size_hint=(0.125, 1))
+        self.up_store.allow_stretch = True
+        self.add_widget(self.up_store)
