3d6fdcf915c095850c248ea23df3b0e408517e2a
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-12-16 16:46:22 +0200

    Add an App subclass to run the game

--- board.py
+++ board.py
@@ -1,3 +1,4 @@
+from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 
@@ -10,3 +11,13 @@ class Board(FloatLayout):
         self.up_store = Image(source='images\\store.png', pos=(0, 0), size_hint=(0.125, 1))
         self.up_store.allow_stretch = True
         self.add_widget(self.up_store)
+
+
+class Mancala(App):
+    def build(self):
+        self.title = 'Mancala'
+        return Board()
+
+
+if __name__ == '__main__':
+    Mancala().run()
