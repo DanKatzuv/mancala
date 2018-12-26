d7de954682d86037b5f8d845251d82ed5a1e2415
Dan Katzuv <dan.dan.kafe@gmail.com>
2018-12-26 09:28:04 +0200

    Make the image cover all area intended for every app size

--- board.py
+++ board.py
@@ -9,7 +9,8 @@ class Board(FloatLayout):
 
     def __init__(self):
         super(Board, self).__init__(size=(1600, 200))
-        self.up_store = Image(source='images\\store.png', pos=(0, 0), size_hint=(0.125, 1))
+        self.up_store = Image(source='images\\store.png', pos_hint={'x': 0, 'center_y': 0.5}, size_hint=(0.125, 1),
+                              allow_strech=True, keep_ratio=False)
         self.up_store.allow_stretch = True
         self.add_widget(self.up_store)
 
